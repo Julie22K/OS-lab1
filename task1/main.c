e8641c3ac376079fd9c4ece012e532983a227b6e
Julie22K <yuliakononenko22@gmail.com>
2022-12-22 20:38:14 +0200

    first commit

---
+++ task1/main.c
@@ -0,0 +1,31 @@
+#include "sys/wait.h"
+#include "stdio.h"
+#include "unistd.h"
+
+int main() {
+    int waitTime = 20;
+
+    if (fork() == 0) {
+      if (fork() == 0) {
+        sleep(waitTime);
+        return 0;
+      }
+    }
+    if (fork() == 0) {
+      if (fork() == 0){
+        sleep(waitTime);
+        return 0;
+      }
+      if (fork() == 0){
+        sleep(waitTime);
+        return 0;
+      }
+    }
+    if (fork() == 0) {
+      sleep(waitTime);
+      return 0;
+    }
+
+    sleep(waitTime);
+    return 0;
+}
